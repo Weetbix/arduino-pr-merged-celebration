2d828849a959de2267ca3b35b6b04a1a419ab76a
John Hannagan <weetbix_@hotmail.com>
2018-09-11 23:39:59 +0200

    Wifi setup basics

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,40 @@
+#include <string>
+
 #include "Arduino.h"
+
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+// Config options for the AP point the
+// duino hosts if it cant connect to wifi.
+const char* ACCESS_POINT_NAME = "PR-Celebration";
+const char* ACCESS_POINT_PW = "mergemaster";
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // Turn on the LED while we are configuring wifi...
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // Attempt to connect to the previous wifi network.
+  // If it's not found, we will instead host an access
+  // point with these details which the user will then
+  // need to connect to.
+  WiFiManager wifiManager;
+  wifiManager.autoConnect(
+    ACCESS_POINT_NAME,
+    ACCESS_POINT_PW
+  );
+
+  // If we get here, we have connected to the wifi.
+  // Turn the LED off.
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  delay(1000);
 }
