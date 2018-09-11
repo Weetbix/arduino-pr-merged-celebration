2af26689a446551034893467b5fe203cb9d580bd
John Hannagan <weetbix_@hotmail.com>
2018-09-11 23:06:59 +0200

    First we blink

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+}
